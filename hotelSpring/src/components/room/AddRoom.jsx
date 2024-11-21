a8422bbdc64963519eb1bbff70111b536c802e74
LeonardHuynh <thienhuuhuynh.dev@gmail.com>
2024-11-21 23:39:16 +0700

    Update Frontend Code

--- hotelSpring/src/components/room/AddRoom.jsx
+++ hotelSpring/src/components/room/AddRoom.jsx
@@ -1,79 +1,132 @@
-import React, { useState } from 'react'
-import {addRoom} from "../utils/APIFunctions";
+import React, { useState } from "react"
+import { addRoom } from "../utils/ApiFunctions"
+import RoomTypeSelector from "../common/RoomTypeSelector"
+import { Link } from "react-router-dom"
 
 const AddRoom = () => {
-  const [newRoom, setNewRoom] = useState({
-    photo: null,
-    roomType: "",
-    roomPrice: ""
-  });
+	const [newRoom, setNewRoom] = useState({
+		photo: null,
+		roomType: "",
+		roomPrice: ""
+	})
 
-  const [successMessage, setSuccessMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [imagePreview, setPreviewImage] = useState("");
+	const [successMessage, setSuccessMessage] = useState("")
+	const [errorMessage, setErrorMessage] = useState("")
+	const [imagePreview, setImagePreview] = useState("")
 
-  // use when user input new infor about room and information of room
-  const handleInputChange = (e) => {
-    const name = e.target.name;
-    let value = e.target.value;
-    if(name === 'roomPrice') {
-      if(!isNaN(value)) {
-        value = parseInt(value);
-      } else {
-        value = "";
-      }
-    }
-    setNewRoom({...newRoom, [name]: value});  // this one will change only the property we are target to
-  }
+	const handleRoomInputChange = (e) => {
+		const name = e.target.name
+		let value = e.target.value
+		if (name === "roomPrice") {
+			if (!isNaN(value)) {
+				value = parseInt(value)
+			} else {
+				value = ""
+			}
+		}
+		setNewRoom({ ...newRoom, [name]: value })
+	}
 
-  const handleImageChange = (e) => {
-    const selectedImage = e.target.files[0];
-    setNewRoom({...newRoom, photo: selectedImage});
-    setPreviewImage(URL.createObjectURL(selectedImage));
-  }
+	const handleImageChange = (e) => {
+		const selectedImage = e.target.files[0]
+		setNewRoom({ ...newRoom, photo: selectedImage })
+		setImagePreview(URL.createObjectURL(selectedImage))
+	}
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const success = await addRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice);
-      if(success !== undefined) {
-        // succes => reset all properties
-        setSuccessMessage("Adding a new Room successfully")
-        setNewRoom({
-          photo: null,
-          roomType: "",
-          roomPrice: ""
-        });
-        setPreviewImage("");
-        setErrorMessage("");
-      } else {
-        setErrorMessage("Error adding a new Room");
-      }
-    } catch (error) {
-      setErrorMessage(error.message);
-    }
-    // after 3 seconds notification will disappear
-    setTimeout(() => {
-      setSuccessMessage("");
-      setErrorMessage("");
-    }, 3000);
-  }
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			const success = await addRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice)
+			if (success !== undefined) {
+				setSuccessMessage("A new room was  added successfully !")
+				setNewRoom({ photo: null, roomType: "", roomPrice: "" })
+				setImagePreview("")
+				setErrorMessage("")
+			} else {
+				setErrorMessage("Error adding new room")
+			}
+		} catch (error) {
+			setErrorMessage(error.message)
+		}
+		setTimeout(() => {
+			setSuccessMessage("")
+			setErrorMessage("")
+		}, 3000)
+	}
 
-  return (
-    <>
-      <section className='container mt-5 mb-5'>
-        <div className='row justify-content-center'>
-          <div className='col-md-8 col-lg-6'>
-            <h2 className='mt-5 mb-2'>Add a new Room</h2>
-              {successMessage && (
-                <div className='alert alert-success fade show'>{successMessage}</div>
-              )}
-              {errorMessage && <div className='alert alert-danger fade show'>{errorMessage}</div>}
-          </div>
-        </div>
-      </section>
-    </>
-  )
+	return (
+		<>
+			<section className="container mt-5 mb-5">
+				<div className="row justify-content-center">
+					<div className="col-md-8 col-lg-6">
+						<h2 className="mt-5 mb-2">Add a New Room</h2>
+						{successMessage && (
+							<div className="alert alert-success fade show"> {successMessage}</div>
+						)}
+
+						{errorMessage && <div className="alert alert-danger fade show"> {errorMessage}</div>}
+
+						<form onSubmit={handleSubmit}>
+							<div className="mb-3">
+								<label htmlFor="roomType" className="form-label">
+									Room Type
+								</label>
+								<div>
+									<RoomTypeSelector
+										handleRoomInputChange={handleRoomInputChange}
+										newRoom={newRoom}
+									/>
+								</div>
+							</div>
+							<div className="mb-3">
+								<label htmlFor="roomPrice" className="form-label">
+									Room Price
+								</label>
+								<input
+									required
+									type="number"
+									className="form-control"
+									id="roomPrice"
+									name="roomPrice"
+									value={newRoom.roomPrice}
+									onChange={handleRoomInputChange}
+								/>
+							</div>
+
+							<div className="mb-3">
+								<label htmlFor="photo" className="form-label">
+									Room Photo
+								</label>
+								<input
+									required
+									name="photo"
+									id="photo"
+									type="file"
+									className="form-control"
+									onChange={handleImageChange}
+								/>
+								{imagePreview && (
+									<img
+										src={imagePreview}
+										alt="Preview  room photo"
+										style={{ maxWidth: "400px", maxHeight: "400px" }}
+										className="mb-3"></img>
+								)}
+							</div>
+							<div className="d-grid gap-2 d-md-flex mt-2">
+								<Link to={"/existing-rooms"} className="btn btn-outline-info">
+									Existing rooms
+								</Link>
+								<button type="submit" className="btn btn-outline-primary ml-5">
+									Save Room
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</section>
+		</>
+	)
 }
 
 export default AddRoom
